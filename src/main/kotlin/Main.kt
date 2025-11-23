508676a5ea0bd9660b58326ed2d6ddde94d678b5
Norskeaksel <ahsorbye@gmail.com>
2025-11-23 19:30:09 +0100

    Add commented graph to main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,17 @@
 import graphMateKT.graphClasses.Graph
 import graphMateKT.graphGraphics.visualizeGraph
-
+/*
+demoGraph={
+    0: [1,2,3],
+    1: [6],
+    2: [5],
+    3: [4],
+    4: [],
+    5: [],
+    6: [],
+    7: []
+}
+*/
 fun main() {
 
 }
