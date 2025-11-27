a152ef07957537b8aecc6ba5dbece0002040a66c
Norskeaksel <ahsorbye@gmail.com>
2025-11-27 12:55:39 +0100

    Restore graphMateKT graph to main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,17 @@
 import graphMateKT.graphClasses.Graph
 import graphMateKT.graphGraphics.visualizeGraph
-/*
-demoGraph={
-    0: [1,2,3],
-    1: [6],
-    2: [5],
-    3: [4],
-    4: [],
-    5: [],
-    6: [],
-    7: []
-}
-*/
-fun main() {
 
+fun main() {
+    val graph = Graph(isWeighted = false)
+    graph.addEdge(0, 1)
+    graph.addEdge(0, 3)
+    graph.addEdge(0, 2)
+    graph.addEdge(1, 6)
+    graph.addEdge(2, 5)
+    graph.addEdge(3, 4)
+    graph.addNode(7)
+
+    graph.bfs(0)
+    graph.visualizeGraph()
+    println(graph.currentVisitedNodes())  // [0, 1, 3, 2, 6, 4, 5]
 }
